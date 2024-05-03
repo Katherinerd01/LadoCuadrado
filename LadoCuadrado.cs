b704b9bf38e5de5a8987e7d84e52787b02cd292e
Katherinerd01 <katherinerdd01@gmail.com>
2024-05-02 20:06:45 -0400

    Lado Cuadrado

---
+++ LadoCuadrado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lado_Cuadrado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Lado, Perimetro;
+            string linea;
+            Console.Write("Ingrese el lado del Cuadrado:");
+            linea = Console.ReadLine();
+            Lado = int.Parse(linea);
+            Perimetro = Lado * 4;
+            Console.Write("El Perimetro del cuadrado es:");
+            Console.Write(Perimetro);
+            Console.ReadKey();
+        }
+    }
+}
